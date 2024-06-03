9db5422b7807e148a3b7cfc71e6d3a6075b2b351
gabriellima-wav <127765401+gabriellima-wav@users.noreply.github.com>
2024-06-03 10:11:17 -0300

    arquivos

--- App.js
+++ App.js
@@ -2,34 +2,43 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import TelaHome from './src/telas/TelaHome'; 
-import TelaLogin from './src/telas/TelaLogin'; 
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-// Import and initialize Firebase
-import { firebase } from '@react-native-firebase/app';
+import TelaHome from './src/telas/TelaHome';
+import TelaLogin from './src/telas/TelaLogin';
+import TelaCadastro from './src/telas/TelaCadastro';
+import TelaInicial from './src/telas/TelaInicial';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDrJa8_QCbxD6_HgvI8TxVOEK8sa8oqKD0",
-  authDomain: "propsaiocean.firebaseapp.com",
-  projectId: "propsaiocean",
-  storageBucket: "propsaiocean.appspot.com",
-  messagingSenderId: "524007923471",
-  appId: "1:524007923471:web:2662803053d793784e287a",
-  measurementId: "G-CC6LW971QN"
-};
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+import firebase from 'firebase/app';
+import { app, db, getFirestore, collection, addDoc, getDocs } from './src/services/firebaseConfig';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={TelaHome} />
-        <Stack.Screen name="Login" component={TelaLogin} />
+      <Stack.Navigator initialRouteName="TelaHome">
+        <Stack.Screen
+          name="TelaHome"
+          component={TelaHome}
+          options={{ headerShown: false }}
+        />
+          <Stack.Screen
+          name="TelaLogin"
+          component={TelaLogin}
+          options={{ headerShown: false }}
+        />
+          <Stack.Screen
+          name="TelaCadastro"
+          component={TelaCadastro}
+          options={{ headerShown: false }}
+        />
+          <Stack.Screen
+          name="TelaInicial"
+          component={TelaInicial}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
